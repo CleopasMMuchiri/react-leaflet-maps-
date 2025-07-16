6873256d0759acd5365f897ff3e466a9ef13e6b6
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2025-07-16 23:18:42 +0300

    Changed maptiler key

--- src/osm_provider.js
+++ src/osm_provider.js
@@ -1,6 +1,6 @@
 export default {
   maptiler: {
-    url: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=iEseH55VAU09RBhAMzTJ",
+    url: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=bonPHvNFQf5vgCBlKoVA",
     attribution:
       '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   },
